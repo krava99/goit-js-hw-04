9a74ab508bed5f8b184a1090388484d0f839032f
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2025-08-20 21:40:14 +0300

    fix index

--- js/task-1.js
+++ js/task-1.js
@@ -8,22 +8,6 @@
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
 
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
-
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-// ); // true
-
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-// ); // false
-
 function isEnoughCapacity(products, containerSize) {
   let total = 0;
   for (const item in products) {
--- js/task-3.js
+++ js/task-3.js
@@ -13,12 +13,6 @@
 // // // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 // console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
-
 const profile = {
   username: 'Jacob',
   playTime: 300,
